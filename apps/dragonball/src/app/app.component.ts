05c1d3a17a696dfd8c7b942c7e157cd1be8068eb
christhodo <102981895+christhodo@users.noreply.github.com>
2022-08-28 17:36:07 -0700

    initial commit

---
+++ apps/dragonball/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dragonball-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'dragonball';
+}
